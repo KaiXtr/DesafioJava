08111dc09903cad34970ee06679790c4cf46845f
Ewerton Bramos <ewertonmatheus2113@gmail.com>
2023-09-30 15:06:01 -0300

    Commit inicial

---
+++ src/main/java/com/udf/desafiojava/DesafioJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.udf.desafiojava;
+
+/**
+ *
+ * @author kaixtr
+ */
+public class DesafioJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
